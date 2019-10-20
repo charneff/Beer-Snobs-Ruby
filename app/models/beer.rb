6787e69c6c8f9e4f43c9ece7e76269d5ba8ff7a0
charneff <charlotte.neff@gmail.com>
2019-10-20 15:28:45 -0500

    add relationships

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,4 +1,7 @@
 class Beer < ApplicationRecord
   belongs_to :user
-  belongs_to :brewery
+  belongs_to :brewery #creator of Beer
+  has_many :reviews
+  has_many :users through: :reviews #those who reviewed it
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  has_many :reviewed_beers, through: :reviews, source: :beer #beers that they reviewed
+  has_many :reviews #created
+  has_many :beers #beers they created
+
 end
