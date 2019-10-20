4c99b1a09ff3bb670fa05c6c00413cb39b3cc5f5
charneff <charlotte.neff@gmail.com>
2019-10-20 15:06:19 -0500

    resource generated

---
+++ db/migrate/20191020200432_create_beers.rb
@@ -0,0 +1,14 @@
+class CreateBeers < ActiveRecord::Migration[6.0]
+  def change
+    create_table :beers do |t|
+      t.string :name
+      t.string :style
+      t.float :abv
+      t.string :flavor_profile
+      t.belongs_to :user, null: false, foreign_key: true
+      t.belongs_to :brewery, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
