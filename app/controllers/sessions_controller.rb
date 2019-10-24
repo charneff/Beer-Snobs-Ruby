17589140c79549ec2560e939e4392039d84355b6
charneff <charlotte.neff@gmail.com>
2019-10-24 07:07:47 -0500

    set up oauth, keys removed

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -15,18 +15,36 @@ class SessionsController < ApplicationController
   end
 
   def new
-  end 
+  end
 
   def create
-    @user = User.find_by(username: params[:user][:username])
-
-    if @user.try(:authenticate, params[:user][:password])
+    if params[:provider] == 'google_oauth2'
+      @user = User.create_by_google_omniauth(auth)
       session[:user_id] = @user.id
       redirect_to user_path(@user)
+
+    elsif params[:provider] == 'github'
+      @user = User.create_by_github_omniauth(auth)
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
+
     else
-      flash[:error] = "Sorry, login info was incorrect. Please try again."
-      redirect_to login_path
+      @user = User.find_by(username: params[:user][:username])
+
+      if @user.try(:authenticate, params[:user][:password])
+        session[:user_id] = @user.id
+        redirect_to user_path(@user)
+      else
+        flash[:error] = "Sorry, login info was incorrect. Please try again."
+        redirect_to login_path
+      end
     end
   end
+
+  private
+
+    def auth
+      request.env['omniauth.auth']
+    end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   post '/signup' => 'users#create'
   delete '/logout' => 'sessions#destroy'
   get '/logout' => 'sessions#destroy'
-
+  get '/auth/:provider/callback' => 'sessions#create'
   resources :reviews
   resources :beers do
     resources :reviews, only: [:new, :index]
